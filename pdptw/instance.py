8c01c870bebdfd8f657305d808a7468c04fd9ce6
gokhanceyhan <gokhanceyhan01@gmail.com>
2022-04-03 11:58:55 +0200

    Initial version of the solver scripts

---
+++ pdptw/instance.py
@@ -0,0 +1,252 @@
+
+import os
+
+from haversine import haversine, Unit
+import numpy as np
+import pandas as pd
+
+from constants import *
+
+
+class Instance:
+
+    def __init__(self):
+        self._capacities = None
+        self._demands = None
+        self._pickup_location_node_indices = None
+        self._delivery_location_node_indices = None
+        self._depot_location_node_index = 0
+        self._distance_matrix = None
+        self._driver_df = None
+        self._driver_id_2_start_location_node_index = {}
+        self._driver_id_2_end_location_node_index = {}
+        self._order_df = None
+        self._order_id_2_pickup_location_node_index = {}
+        self._order_id_2_delivery_location_node_index = {}
+        self._pickup_and_deliveries = None
+        self._time_windows = None
+
+        self._read_drivers()
+        self._read_orders()
+        self._create_capacitites()
+        self._create_pickup_nodes()
+        self._create_delivery_nodes()
+        self._create_demands()
+        self._create_distance_matrix()
+        self._create_pickup_and_deliveries()
+        self._create_time_windows()
+
+    def _create_capacitites(self):
+        num_drivers = len(self._driver_df)
+        self._capacities = [DRIVER_CAPACITY] * num_drivers
+
+    def _create_pickup_nodes(self):
+        num_drivers = len(self._driver_df)
+        num_orders = len(self._order_df)
+        self._pickup_location_node_indices = [
+            i + 2 * num_drivers + 1 for i in range(num_orders)]
+
+    def _create_delivery_nodes(self):
+        num_drivers = len(self._driver_df)
+        num_orders = len(self._order_df)
+        self._delivery_location_node_indices = [
+            i + num_orders + 2 * num_drivers + 1 for i in range(num_orders)]
+
+    def _create_demands(self):
+        num_drivers = len(self._driver_df)
+        num_orders = len(self._order_df)
+        # zero demand for the depot
+        demands = [0]
+        # zero demand for the driver start / end locations
+        demands.extend([0] * num_drivers * 2)
+        # positive demand for order pickup num_locations
+        pickup_demands = list(self._order_df["no_of_items"])
+        demands.extend(pickup_demands)
+        delivery_demands = list(-1 * self._order_df["no_of_items"])
+        demands.extend(delivery_demands)
+        self._demands = demands
+
+    def _create_distance_matrix(self):
+        num_drivers = len(self._driver_df)
+        num_orders = len(self._order_df)
+        big_m = 1_000_000
+        distances = []
+        # add the distances from the central depot
+        location_node_index = 0
+        from_depot_to_depot = [big_m]
+        from_depot_to_driver_start_locations = [0] * num_drivers
+        from_depot_to_driver_end_locations = [big_m] * num_drivers
+        from_depot_to_order_locations = [big_m] * num_orders * 2
+        from_depot = from_depot_to_depot + from_depot_to_driver_start_locations + \
+            from_depot_to_driver_end_locations + from_depot_to_order_locations
+        distances.append(from_depot)
+        # add distances from the driver locations
+        location_node_index += 1
+        for driver_id, driver in self._driver_df.iterrows():
+            # for driver start location
+            self._driver_id_2_start_location_node_index[driver_id] = location_node_index
+            start_location = (driver["start_location_lat"], driver["start_location_long"])
+            # to depot
+            distances_ = [big_m]
+            # to driver start locations
+            distances_.extend([big_m] * num_drivers)
+            # to driver end locations
+            distances_.extend([0] * num_drivers)
+            # to order pickup locations
+            for order_id, order in self._order_df.iterrows():
+                pickup_location = (order["restaurant_lat"], order["restaurant_long"])
+                distance = haversine(start_location, pickup_location, unit=Unit.METERS)
+                distances_.append(int(distance))
+            # to order end locations
+            distances_.extend([big_m] * num_orders)
+            distances.append(distances_)
+            # increment the location node index
+            location_node_index += 1
+        for driver_id, driver in self._driver_df.iterrows():
+            # for driver end location
+            self._driver_id_2_end_location_node_index[driver_id] = location_node_index
+            # to depot
+            distances_ = [0]
+            # to all driver and order locations
+            distances_.extend([big_m] * (num_drivers + num_orders) * 2)
+            distances.append(distances_)
+            # increment the location node index
+            location_node_index += 1
+
+        # add distances from the order locations
+        for order_id, order in self._order_df.iterrows():
+            # for pickup location
+            self._order_id_2_pickup_location_node_index[order_id] = location_node_index
+            pickup_location = (order["restaurant_lat"], order["restaurant_long"])
+            # to depot
+            distances_ = [big_m]
+            # to driver start locations
+            distances_.extend([big_m] * num_drivers * 2)
+            # to order locations
+            for order_id_, order_ in self._order_df.iterrows():
+                pickup_location_ = (order_["restaurant_lat"], order_["restaurant_long"])
+                distance = haversine(pickup_location, pickup_location_, unit=Unit.METERS)
+                distances_.append(int(distance))
+            for order_id_, order_ in self._order_df.iterrows():
+                delivery_location_ = (order_["customer_lat"], order_["customer_long"])
+                distance = haversine(pickup_location, delivery_location_, unit=Unit.METERS)
+                distances_.append(int(distance))
+            distances.append(distances_)
+            # increment the location node index
+            location_node_index += 1
+        for order_id, order in self._order_df.iterrows():
+            # for delivery location
+            self._order_id_2_delivery_location_node_index[order_id] = location_node_index
+            delivery_location = (order["customer_lat"], order["customer_long"])
+            # to depot
+            distances_ = [big_m]
+            # to driver start locations
+            distances_.extend([big_m] * num_drivers)
+            # to driver end locations
+            distances_.extend([0] * num_drivers)
+            # to order locations
+            for order_id_, order_ in self._order_df.iterrows():
+                pickup_location_ = (order_["restaurant_lat"], order_["restaurant_long"])
+                distance = haversine(delivery_location, pickup_location_, unit=Unit.METERS)
+                distances_.append(int(distance))
+            for order_id_, order_ in self._order_df.iterrows():
+                delivery_location_ = (order_["customer_lat"], order_["customer_long"])
+                distance = haversine(delivery_location, delivery_location_, unit=Unit.METERS)
+                distances_.append(int(distance))
+            distances.append(distances_)
+            # increment the location node index
+            location_node_index += 1
+        self._distance_matrix = np.asarray(distances)
+
+    def _create_pickup_and_deliveries(self):
+        pickup_and_deliveries = []
+        # for driver start and end locations
+        for driver_id in self._driver_df.index.values:
+            start = self._driver_id_2_start_location_node_index[driver_id]
+            end = self._driver_id_2_end_location_node_index[driver_id]
+            pickup_and_deliveries_ = [start, end]
+            pickup_and_deliveries.append(pickup_and_deliveries_)
+        # for regular order pickup and delivery locations
+        for order_id in self._order_df.index.values:
+            pickup_and_delivery = []
+            pickup = self._order_id_2_pickup_location_node_index[order_id]
+            delivery = self._order_id_2_delivery_location_node_index[order_id]
+            pickup_and_delivery.append(pickup)
+            pickup_and_delivery.append(delivery)
+            pickup_and_deliveries.append(pickup_and_delivery)
+        self._pickup_and_deliveries = pickup_and_deliveries
+
+    def _create_time_windows(self):
+        time_windows = []
+        start_time = 0
+        end_time = 10_000_000
+        # for the depot
+        depot_time_window = (start_time, end_time)
+        time_windows.append(depot_time_window)
+        # for the driver start locations
+        for shift_start_time in list(self._driver_df["shift_start_sec"]):
+            time_windows.append((shift_start_time, end_time))
+        # for the driver end locations
+        for shift_end_time in list(self._driver_df["shift_end_sec"]):
+            time_windows.append((start_time, shift_end_time))
+        # for order pickup locations
+        for pickup_time in list(self._order_df["prep_duration_sec"]):
+            time_windows.append((pickup_time, end_time))
+        # for order delivery locations
+        for delivery_time in list(self._order_df["preferred_otd_sec"]):
+            time_windows.append((start_time, delivery_time))
+        self._time_windows = time_windows
+
+    def _read_drivers(self):
+        path = os.path.join(WORKING_DIR, DRIVER_FILE_NAME)
+        self._driver_df = pd.read_csv(path).set_index("driver_id")
+
+    def _read_orders(self):
+        path = os.path.join(WORKING_DIR, ORDER_FILE_NAME)
+        self._order_df = pd.read_csv(path).set_index("order_id")
+
+    def capacities(self):
+        return self._capacities
+
+    def pickup_location_node_indices(self):
+        return self._pickup_location_node_indices
+
+    def delivery_location_node_indices(self):
+        return self._delivery_location_node_indices
+
+    def depot_location_node_index(self):
+        return self._depot_location_node_index
+
+    def demands(self):
+        return self._demands
+
+    def distance_matrix(self):
+        return self._distance_matrix
+
+    def driver_df(self):
+        return self._driver_df
+
+    def driver_id_2_start_location_node_index(self):
+        return self._driver_id_2_start_location_node_index
+
+    def driver_id_2_end_location_node_index(self):
+        return self._driver_id_2_end_location_node_index
+
+    def order_df(self):
+        return self._order_df
+
+    def pickup_and_deliveries(self):
+        return self._pickup_and_deliveries
+
+    def print(self):
+        print(self._driver_df())
+        print(self._order_df())
+        print(self._delivery_location_node_indices())
+        print(self._distance_matrix())
+        print(self._capacities())
+        print(self._demands())
+        print(self._pickup_and_deliveries())
+        print(self._time_windows())
+
+    def time_windows(self):
+        return self._time_windows
---
+++ pdptw/solver.py
@@ -0,0 +1,229 @@
+"""Implements a solver for the PDPTW by using the routing solver of Google OR Tools"""
+
+import math
+
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+
+from constants import *
+from instance import Instance
+
+
+class Solver:
+
+    def __init__(self, instance):
+        self._instance = instance
+
+    def _print_solution(self, manager, routing, solution):
+        """Prints the solution on console"""
+        num_drivers = len(self._instance.driver_df())
+        total_distance = 0
+        max_route_distance = 0
+        total_travel_time = 0
+        max_route_travel_time = 0
+        max_pickup_distance_in_meters = 0
+        late_deliveries = []
+        time_dimension = routing.GetDimensionOrDie('Time')
+        distance_dimension = routing.GetDimensionOrDie('Distance')
+        for vehicle_id in range(num_drivers):
+            index = routing.Start(vehicle_id)
+            plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+            route_distance = 0
+            route_load = 0
+            while not routing.IsEnd(index):
+                time_var = time_dimension.CumulVar(index)
+                node_index = manager.IndexToNode(index)
+                route_load += self._instance.demands()[node_index]
+                plan_output += ' {0} Load({1}) Time({2},{3}) -> '.format(
+                    node_index, route_load, solution.Min(time_var),
+                    solution.Max(time_var))
+                previous_index = index
+                index = solution.Value(routing.NextVar(index))
+                route_distance += distance_dimension.GetTransitValue(previous_index, index, vehicle_id)
+                delivery_node = node_index in self._instance.delivery_location_node_indices()
+                if delivery_node:
+                    target_delivery_time = self._instance.time_windows()[node_index][1]
+                    earliest_delivery_time = solution.Min(time_var)
+                    if  earliest_delivery_time > target_delivery_time:
+                        late_deliveries.append((node_index, earliest_delivery_time - target_delivery_time))
+                else:
+                    max_pickup_distance_in_meters = max(
+                        max_pickup_distance_in_meters, 
+                        distance_dimension.GetTransitValue(previous_index, index, vehicle_id))
+            # handle the last node visited
+            time_var = time_dimension.CumulVar(index)
+            node_index = manager.IndexToNode(index)
+            delivery_node = node_index in self._instance.delivery_location_node_indices()
+            if delivery_node:
+                target_delivery_time = self._instance.time_windows()[node_index][1]
+                earliest_delivery_time = solution.Min(time_var)
+                if  earliest_delivery_time > target_delivery_time:
+                    late_deliveries.append((node_index, earliest_delivery_time - target_delivery_time))
+            plan_output += ' {0} Load({1}) Time({2},{3}) \n'.format(
+                manager.IndexToNode(index), route_load, solution.Min(time_var),
+                solution.Max(time_var))
+            plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+            plan_output += 'Load of the route: {}\n'.format(route_load)
+            plan_output += 'Time of the route: {}sec\n'.format(solution.Min(time_var))
+            print(plan_output)
+            total_distance += route_distance
+            max_route_distance = max(max_route_distance, route_distance)
+            route_travel_time = route_distance / (1000 * DRIVER_SPEED) * 3600
+            total_travel_time += route_travel_time
+            max_route_travel_time = max(max_route_travel_time, route_travel_time)
+        print('Total distance of all routes: {}m'.format(total_distance))
+        print('Max route distance: {}m'.format(max_route_distance))
+        print('Max pickup distance: {}m'.format(max_pickup_distance_in_meters))
+        print('Total travel time of all routes: {}sec'.format(int(total_travel_time)))
+        print('Max route travel time: {}sec'.format(int(max_route_travel_time)))
+        print('Number of late deliveries: {}'.format(len(late_deliveries)))
+        delayed_nodes = [late_delivery[0] for late_delivery in late_deliveries]
+        print('Delayed nodes: {}'.format(delayed_nodes))
+        delays = [late_delivery[1] for late_delivery in late_deliveries]
+        print('Delays: {}'.format(delays))
+        total_delay = sum(delays)
+        print('Total delay: {}sec'.format(total_delay))
+
+    def run(self):
+        """Creates the routing problem and solves it"""
+        big_m = 1_000_000
+        num_nodes = len(self._instance.distance_matrix())
+        num_drivers = len(self._instance.driver_df())
+        print(f"{num_nodes} nodes with {num_drivers} drivers")
+        depot_node_index = 0
+
+        # Create the routing index manager
+        manager = pywrapcp.RoutingIndexManager(num_nodes, num_drivers, depot_node_index)
+        # Create Routing Model
+        routing = pywrapcp.RoutingModel(manager)
+
+        def distance_callback(from_index, to_index):
+            """Returns the distance between the two nodes"""
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return self._instance.distance_matrix()[from_node][to_node]
+
+        distance_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+        dimension_name = 'Distance'
+        routing.AddDimension(
+            distance_callback_index,
+            0,  # no slack
+            big_m,  # vehicle maximum travel distance
+            True,  # start cumul to zero
+            dimension_name)
+        distance_dimension = routing.GetDimensionOrDie(dimension_name)
+
+        def demand_callback(from_index):
+            """Returns the demand of the node"""
+            # Convert from routing variable Index to demands NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            return self._instance.demands()[from_node]
+
+        demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+        routing.AddDimensionWithVehicleCapacity(
+            demand_callback_index,
+            0,  # null capacity slack
+            self._instance.capacities(),  # vehicle maximum capacities
+            True,  # start cumul to zero
+            'Capacity')
+
+        for request in self._instance.pickup_and_deliveries():
+            pickup_index = manager.NodeToIndex(request[0])
+            delivery_index = manager.NodeToIndex(request[1])
+            # an order must be served by the same driver
+            # driver start and end location visits are also modeled as dummy pickup and delivery
+            routing.AddPickupAndDelivery(pickup_index, delivery_index)
+            routing.solver().Add(
+                routing.VehicleVar(pickup_index) == routing.VehicleVar(
+                    delivery_index))
+            # pickup before delivery
+            routing.solver().Add(
+                distance_dimension.CumulVar(pickup_index) <=
+                distance_dimension.CumulVar(delivery_index))
+            # max pickup distance hard constraint
+            routing.solver().Add(distance_dimension.TransitVar(pickup_index) <= MAX_PICKUP_DISTANCE_IN_METERS)
+            
+
+        def time_callback(from_index, to_index):
+            """Returns the travel time between the two nodes."""
+            # Convert from routing variable Index to time matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            distance_in_m = self._instance.distance_matrix()[from_node][to_node]
+            distance_in_km = distance_in_m / 1000
+            time_in_h = distance_in_km / DRIVER_SPEED
+            time_in_s = time_in_h * 3600
+            return int(time_in_s)
+
+        time_callback_index = routing.RegisterTransitCallback(time_callback)
+
+        time = 'Time'
+        routing.AddDimension(
+            time_callback_index,
+            big_m,  # allow waiting time
+            big_m,  # maximum time per vehicle
+            False,  # Don't force start cumul to zero.
+            time)
+        time_dimension = routing.GetDimensionOrDie(time)
+
+        # Add time window constraints for each location except depot.
+        for location_node_index, time_window in enumerate(self._instance.time_windows()):
+            if location_node_index == 0:
+                continue
+            index = manager.NodeToIndex(location_node_index)
+            lb = time_window[0]
+            ub = time_window[1]
+            if location_node_index in self._instance.delivery_location_node_indices():
+                time_dimension.CumulVar(index).SetMin(lb)
+                time_dimension.SetCumulVarSoftUpperBound(index, ub, LATE_DELIVERY_PER_SEC_PENALTY)
+            else:
+                time_dimension.CumulVar(index).SetRange(lb, ub)
+
+        # Define cost of each arc
+        def arc_cost_callback(from_index, to_index):
+            """Returns the arc cost"""
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            distance = self._instance.distance_matrix()[from_node][to_node]
+            long_distance_penalty = ABOVE_MAX_DISTANCE_PER_METER_PENALTY * max(0, distance - SOFT_MAX_DISTANCE_IN_METERS)
+            return distance + long_distance_penalty
+
+        arc_cost_callback_index = routing.RegisterTransitCallback(arc_cost_callback)
+        routing.SetArcCostEvaluatorOfAllVehicles(arc_cost_callback_index)
+
+        for i in range(num_drivers):
+            routing.AddVariableMinimizedByFinalizer(
+                time_dimension.CumulVar(routing.Start(i)))
+            routing.AddVariableMinimizedByFinalizer(
+                time_dimension.CumulVar(routing.End(i)))
+
+        # Setting search parameters
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.AUTOMATIC)
+        search_parameters.local_search_metaheuristic = (
+            routing_enums_pb2.LocalSearchMetaheuristic.AUTOMATIC)
+        search_parameters.time_limit.seconds = 30
+        search_parameters.log_search = True
+
+        # Solve the problem
+        solution = routing.SolveWithParameters(search_parameters)
+        print("Solver status: ", routing.status())
+
+        # Print solution on console
+        if solution:
+            self._print_solution(manager, routing, solution)
+        else:
+            print('No solution found !')
+
+
+def main():
+    instance = Instance()
+    solver = Solver(instance)
+    solver.run()
+
+
+if __name__ == "__main__":
+    main()
